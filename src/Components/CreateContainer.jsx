76c0e17d86a0ce5487e7d561081e895bd6c5af62
Sohel51 <sohelcutyy@gmail.com>
2022-11-12 20:15:18 +0600

    Upload $ Recive the Data in Firebase

--- src/Components/CreateContainer.jsx
+++ src/Components/CreateContainer.jsx
@@ -3,6 +3,9 @@ import { motion } from 'framer-motion';
 import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from 'react-icons/md';
 import { categories } from '../utils/data';
 import Loader from './Loader';
+import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../firebase.config';
+import { saveItem } from '../utils/firebaseFunction';
 
 const CreateContainer = () => {
 
@@ -15,9 +18,97 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const uploadImage = () => {};
-  const deleteImage = () => {};
-  const saveDetails = () => {};
+
+  const uploadImage = (e) => {
+    setIsLoading(true);
+    const imageFile = e.target.files[0];
+    const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const uploadProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    }, (error) => {
+      console.log(error);
+      setFields(true);
+      setMsg('Error while uploading : Try Again');
+      setAlertStatus('danger');
+      setTimeout(() => {
+        setFields(false)
+        setIsLoading(false)
+      }, 4000);
+    }, () => {
+      getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+        setImageAsset(downloadURL);
+        setIsLoading(false)
+        setFields(true);
+        setMsg('Image Upload Successfully');
+        setAlertStatus('success')
+        setTimeout(() => {
+          setFields(false)
+        }, 4000);
+      })
+    })
+  };
+
+  const deleteImage = () => {
+    setIsLoading(true);
+    const deleteRef = ref(storage, imageAsset);
+    deleteObject(deleteRef).then(() => {
+      setImageAsset(null);
+      setIsLoading(false);
+      setFields(true);
+      setMsg('Image Deleted Successfully');
+      setAlertStatus('success');
+      setTimeout(() => {
+        setFields(false);
+      }, 4000);
+    })
+  };
+
+  const saveDetails = () => {
+    setIsLoading(true);
+    try {
+      if (!title || !calories || !imageAsset || !price || !category) {
+        setFields(true);
+        setMsg("Required fields can't be empty");
+        setAlertStatus("danger");
+        setTimeout(() => {
+          setFields(false);
+          setIsLoading(false);
+        }, 4000);
+      } else {
+        const data = {
+          id: `$Date.now()`, title: title, imageURL: imageAsset, category: category, calories: calories, qty: 1, price: price,
+        }
+        saveItem(data)
+        setIsLoading(false);
+        setFields(true);
+        setMsg('Data Uploaded Successfully');
+        clearData();
+        setAlertStatus('success');
+        setTimeout(() => {
+          setFields(false);
+        }, 4000);
+      }
+    } catch (error) {
+      console.log(error);
+      setFields(true);
+      setMsg('Error while uploading : Try Again');
+      setAlertStatus('danger');
+      setTimeout(() => {
+        setFields(false)
+        setIsLoading(false)
+      }, 4000);
+    }
+  };
+
+  const clearData = () => {
+    setTitle("");
+    setImageAsset(null);
+    setCalories("");
+    setPrice("");
+    setCalories("Select Category");
+  };
 
   return (
     <div className='w-full min-h-screen flex items-center justify-center'>
@@ -83,7 +174,7 @@ const CreateContainer = () => {
         </div>
 
         <div className='flex items-center w-full'>
-            <button type='button' className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold' onClick={saveDetails}>Save</button>
+          <button type='button' className='ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold' onClick={saveDetails}>Save</button>
         </div>
       </div>
     </div>

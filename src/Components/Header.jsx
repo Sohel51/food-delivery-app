c8126ca92e175ded5e1ca7ad6dabe13e866c80c6
Sohel51 <sohelcutyy@gmail.com>
2023-02-04 19:16:45 +0600

    Updates

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -137,7 +137,7 @@ const Header = () => {
                   user && user.email === "sohelcutyy@gmail.com" && (
                     <Link to={'/CreateContainer'}>
                       <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>
-                        New Item <MdAdd />
+                        New Item Added<MdAdd />
                       </p>
                     </Link>
                   )
